52664609905255996cc8b115bbfec70dc32cc6ef
Matheus <matheustts8@gmail.com>
2022-02-14 00:30:49 -0300

    Customizando a navegação

---
+++ Algorhythm.Frontend/front-end/src/app/navigation/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+    public isCollapsed: boolean;
+  
+    constructor() {
+      this.isCollapsed = true;
+    }
+}
